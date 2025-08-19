762518f1c88772b6e21e4d1495aa4122219c718d
Marcelo Eiji Kuwamoto <eiji.mek@gmail.com>
2025-08-19 18:24:39 -0300

    refactor: remove blog posts, authors, and tags for site cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,7 +93,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
         ],
       },
       footer: {
@@ -128,10 +128,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
